8da570c04cbb2f22165dde05dda8bf1753e0883a
leemm0000 <mm85471211@gmail.com>
2025-06-17 15:45:27 +0900

    Update custom.js

--- script/custom.js
+++ script/custom.js
@@ -7,7 +7,7 @@ fetch('경로')
 })
 */
 
-fetch('/include/footer.html')
+fetch('./include/footer.html')
 .then(response => response.text())
 .then(data => {
   document.querySelector('.footer-include').innerHTML = data;
@@ -27,7 +27,7 @@ fetch('/include/footer.html')
   })
 });
 
-fetch('/include/header.html')
+fetch('./include/header.html')
 .then(response => response.text())
 .then(data => {
   document.querySelector('.header-include').innerHTML = data;
